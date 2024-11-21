eb2a293f0a597c1978ec1d2e810bc3cc62cf3531
wanguimbutu <wanguimbutu@gmail.com>
2024-11-21 15:58:33 +0300

    changes

---
+++ frontend/src/components/LoginForm.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <input type="text" placeholder="username" v-model="username"/>
+        <input type="text" placeholder="password" v-model="password"/>
+        <button type="submit">Login</button>
+    </div>
+</template>
+<script>
+export default{
+    data(){
+        return{
+            username: '',
+            password: ''
+        };
+    },
+    methods: {
+        login(){
+
+        }
+    }
+}
+</script>
---
+++ frontend/src/pages/Login.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="login-form">
+        <div class="p-2">
+            <input type="text" placeholder="username" v-model="username"/>
+        </div>
+
+        <div class="p-2">
+            <input type="text" placeholder="password" v-model="password"/>
+        </div>
+         <Button
+            :variant="'solid'"
+            :ref_for="true" theme="gray" size="sm" label="Button"
+            :loading="false"
+            :loadingText="null"
+            :disabled="false"
+            :link="null"
+            >
+            Login
+        </Button>
+        </div>
+</template>
+<script>
+export default{
+    data(){
+        return{
+            username: '',
+            password: ''
+        };
+    },
+    methods: {
+        login(){
+
+        }
+    }
+}
+</script>
