610668159ad97e3486dd28828403cb1fd9aa7f5e
wangui <92292540+wanguimbutu@users.noreply.github.com>
2024-12-20 14:49:36 +0300

    Update FrappeApi.js

--- frontend/src/utils/FrappeApi.js
+++ frontend/src/utils/FrappeApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const frappe = axios.create({
-  baseURL: "https://erpnext-181781-0.cloudclusters.net//api/resource",
+  baseURL: "",
   withCredentials: true,
 });
